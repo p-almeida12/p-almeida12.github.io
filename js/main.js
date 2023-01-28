8639393ac10e556b0e198d463464a4b3bcc6daef
Paulo Almeida <109768075+p-almeida12@users.noreply.github.com>
2023-01-28 02:05:10 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function openAboutMePage(){
+    document.getElementById("content").innerHTML = "";
+    document.getElementById("content").innerHTML='<object type="text/html" data="../html/aboutMePage.html" ></object>';
+}
