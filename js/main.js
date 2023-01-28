909a974ca1e2ad6edfdf9bd8aa5cab1a084276ee
Paulo Almeida <109768075+p-almeida12@users.noreply.github.com>
2023-01-28 02:44:39 +0000

    Delete js directory

--- js/main.js
+++
@@ -1,4 +0,0 @@
-function openAboutMePage(){
-    document.getElementById("content").innerHTML = "";
-    document.getElementById("content").innerHTML='<object type="text/html" data="../html/aboutMePage.html" ></object>';
-}
